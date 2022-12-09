99fdd245e3eecae75d5a232f2ea0214c363fce9e
toddbrentlinger <toddbrentlinger@gmail.com>
2022-12-09 13:15:49 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s(a|c)ss)$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader',],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults', }, ],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+};
